d9ffd1c6aac57aed4193d72d5a8fffac1ab28073
OmarEhab14 <www.omarehab3710@gmail.com>
2025-08-12 09:33:32 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> add
+                = (a, b) -> a + b;
+        System.out.println(add.apply(3, 4));
+
+        Consumer<String> sendWelcomeMail = mail ->
+                System.out.println("MAILING " + mail);
+        register("hany", sendWelcomeMail);
+
+        Function<Integer, Integer> doubleNumber = multiplyBy(2);
+        Function<Integer, Integer> tripleNumber = multiplyBy(3);
+        System.out.println(doubleNumber.apply(4));
+        System.out.println(tripleNumber.apply(4));
+        System.out.println(multiplyBy(5).apply(6));
+
+    }
+    public static void register(String name, Consumer<String> callback) {
+        callback.accept(name + "domain.com");
+    }
+    public static Function<Integer, Integer> multiplyBy(int x) {
+        return y -> x * y;
+    }
+}
